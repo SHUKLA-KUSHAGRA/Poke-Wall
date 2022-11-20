50c2d29e97ae41c7a4bca09cb8fd8906d9814c19
Kushagra Shukla <shukla.kushagra.business@gmail.com>
2022-11-21 00:17:47 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const url="https://pokeapi.co/api/v2/pokemon?limit=800";
+
 function App() {
+
+  const [ names , setNames ] = useState([]);
+
+  async function getDataFromApi(){
+      const api = await axios.get(url);
+      const data = api.data.results;
+      setNames(data.map((elem)=>{
+        return elem.name;
+      }));
+  }
+
+  useEffect(()=>{
+      getDataFromApi();
+  },[]);
+  
+  async function getImage(event){
+    const x = parseInt(event.target.id);
+    const imgurl = `https://pokeapi.co/api/v2/pokemon/${x+1}`;
+    const api = await axios.get(imgurl);
+    const data = api.data.sprites.front_default;
+    window.open(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Pokedex</h1>
+      <ol>
+      {names.map((val,idx)=>{
+        return <li key={idx} id={idx} onClick={getImage}>{val}</li>
+      })}
+      </ol>
+    </>
   );
 }
 
